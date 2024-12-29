9f680170461cc2eaeb18861c1d0e2d37869693ef
Klaidoscope14 <saagarchaitanya80@gmail.com>
2024-12-29 23:22:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Problem12 from './Problem12';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <Problem12/>
+  </React.StrictMode>
+);
